99dc0da8f6ce47c5ff09be6484904e6aeae7df64
Tiba Gabor <tibag7@gmail.com>
2016-03-30 14:34:20 +0200

    In ProcessManager class ElementFromSystemProcesses method edited

--- ProcessNote/Controller/ProcessManager.cs
+++ ProcessNote/Controller/ProcessManager.cs
@@ -13,7 +13,6 @@ namespace ProcessNote.Controller
     {
         private List<SystemProcess> systemProcesses;
         
-
         public ProcessManager()
         {
             this.systemProcesses = SystemProcesses;
@@ -34,6 +33,7 @@ namespace ProcessNote.Controller
       
         private SystemProcess ElementFromSystemProcesses(int id)
         {
+            this.systemProcesses = SystemProcesses;
             foreach (SystemProcess systemProcess in systemProcesses)
             {
                 if (systemProcess.Id == id)
