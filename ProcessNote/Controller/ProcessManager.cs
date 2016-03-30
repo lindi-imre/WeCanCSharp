7f182493ce433a19757655fc43fa2846f1c40a7a
subertbalazs <balazs.subert@gmail.com>
2016-03-30 13:48:50 +0200

    Implemented GetProcesses method

---
+++ ProcessNote/Controller/ProcessManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using ProcessNote.Model;
+
+namespace ProcessNote.Controller
+{
+    class ProcessManager
+    {
+        public List<SystemProcess> GetProcessesFromSystem()
+        {
+            List<SystemProcess> resultList = new List<SystemProcess>();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                resultList.Add((SystemProcess) process);
+            }
+            return resultList;
+        }
+
+        public SystemProcess GetProcessInfo(int id)
+        {
+            return null;
+        }
+
+        public ProcessThreadCollection GetThreadsInfo(int id)
+        {
+            return null;
+        }
+    }
+}
