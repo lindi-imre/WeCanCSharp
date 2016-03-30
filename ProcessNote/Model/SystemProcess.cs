cbd6080a107d5dcc8818818346f2b9373a4f015b
Tiba Gabor <tibag7@gmail.com>
2016-03-30 13:15:37 +0200

    SystemProcess class implemented

---
+++ ProcessNote/Model/SystemProcess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessNote.Model
+{
+    public class SystemProcess: Process
+    {
+        private string comment;
+
+        public SystemProcess()
+        {
+            Comment = "";
+        }
+
+        public string Comment
+        {
+            get
+            {
+                return comment;
+            }
+
+            set
+            {
+                comment = value;
+            }
+        }
+    }
+}
